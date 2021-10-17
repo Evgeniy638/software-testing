741deed1712b0e24583ce734c01edfa254a64760
Evgeniy <56478221+Evgeniy638@users.noreply.github.com>
2021-10-17 17:11:05 +0300

    add check main slider

--- practice4/src/test/java/tests/BaseTest.java
+++ practice4/src/test/java/tests/BaseTest.java
@@ -5,7 +5,7 @@ import org.openqa.selenium.chrome.ChromeDriver;
 import org.testng.annotations.AfterClass;
 import org.testng.annotations.BeforeClass;
 import org.testng.annotations.BeforeMethod;
-import pages.HomePage;
+import pages.home.HomePage;
 import utils.logs.Log;
 
 public class BaseTest {
