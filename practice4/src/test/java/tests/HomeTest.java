3faddf76bccbdae7fd6df0cb6790de26177d847e
Danil <shambonik@gmail.com>
2021-10-19 00:08:33 +0300

    Tests of preorders list, navigation menu and categories list added

--- practice4/src/test/java/tests/HomeTest.java
+++ practice4/src/test/java/tests/HomeTest.java
@@ -48,4 +48,41 @@ public class HomeTest extends BaseTest {
                 .goToSalesLeaders()
                 .checkHover();
     }
+
+    @Test(priority = 0, description = "Проверка календаря релизов на главной странице")
+    @Severity(SeverityLevel.BLOCKER)
+    @Description("Переключение по страницам списка и проверка стилей при наведении на элементы в списке")
+    @Story("Пользователь смотрит, когда выйдут новые игры")
+    public void checkMainPreordersList(Method method) {
+        startTest(method.getName(), "Проверка календаря релизов на главной странице");
+        homePage.goToHome()
+                .goToPreordersList()
+                .checkContent()
+                .checkNextButton()
+                .checkPrevButton();
+    }
+
+    @Test(priority = 0, description = "Проверка категорий на главной странице")
+    @Severity(SeverityLevel.BLOCKER)
+    @Description("Проверка работы кнопки \"Показать все\", проверка стилей при наведении на элементы в списке")
+    @Story("Пользователь просматривает категории игр")
+    public void checkMainCategories(Method method) {
+        startTest(method.getName(), "Проверка категорий на главной странице");
+        homePage.goToHome()
+                .goToCategories()
+                .checkCategoryItems()
+                .checkShowMoreButton()
+                .checkCategoryItems();
+    }
+
+    @Test(priority = 0, description = "Проверка навигации на главной странице")
+    @Severity(SeverityLevel.BLOCKER)
+    @Description("Проверка работы меню навигации, правильности ссылок")
+    @Story("Пользователь перемещается между страницами с помощью меню навигации")
+    public void checkMainNavigation(Method method) {
+        startTest(method.getName(), "Проверка навигации на главной странице");
+        homePage.goToHome()
+                .goToNavigation()
+                .checkNavigation(0);
+    }
 }
